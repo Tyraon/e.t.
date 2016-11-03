7be42625917ff73abdb136aa236da3190c2212de
Sven Kästel <osis@outlook.de>
2016-11-03 11:29:35 +0100

    Profile Update
    Das Benutzerprofil kann nun vom Nutzer aktualisiert werden.

--- inc/profile.php
+++ inc/profile.php
@@ -1,13 +1,19 @@
 <?php
-
+$errmsg;
 if($_GET['a'] == "1") {
-	$db = @mysql_connect($config['db_host'],$config['user'],$config['pass']);
-	@mysql_select_db($config['db_name']);
-	@mysql_query("UPDATE `et_user` SET `username` = '{$_POST['user']}', `last_name` = '{$_POST['lastname']}', `first_name` = '{$_POST['firstname']}', `email` = '{$_POST['email']}' WHERE `id` = '{$_SESSION['et_uid']}'");
 	if($_POST['pass'] != "" && $_POST['pass'] == $_POST['pass2']) {
-		@mysql_query("UPDATE `et_user`SET `user_pass` = '{$_POST['pass']}' WHERE `id` = '{$_SESSION['et_uid']}'");
+		$db = @mysql_connect($config['db_host'],$config['db_user'],$config['db_pass']) OR DIE ("Verbindung nicht möglich!");
+		@mysql_select_db($config['db_name']) OR DIE ("Datenbank nicht gefunden!".mysql_error());
+		$pass = sha1(strtoupper($_POST['user']).':'.strtoupper($_POST['pass']));
+		@mysql_query("UPDATE `et_user` SET `username` = '{$_POST['user']}', `last_name` = '{$_POST['lastname']}', `first_name` = '{$_POST['firstname']}', `email` = '{$_POST['email']}', `userpass` = '".$pass."' WHERE `id` = '{$_SESSION['et_uid']}'");
+		$_SESSION['et_user'] = $_POST['user'];
+		$_SESSION['et_lastname'] = $_POST['lastname'];
+		$_SESSION['et_firstname'] = $_POST['firstname'];
+		$_SESSION['et_email'] = $_POST['email'];
+		@mysql_close($db);
+	} else {
+		$errmsg = '<font color="#c00">Die Passw&ouml;rter m&uuml;ssem identisch sein!</font>';
 	}
-	@mysql_close($db);
 }
 
 switch($_SESSION['et_lvl']) {
@@ -27,7 +33,7 @@ switch($_SESSION['et_lvl']) {
 ?>
 
 <div class="content">
-	<div id="dashboard">
+	<div id="dashboard"><?= $errmsg;?>
         <table border="0">
         <form action="index.php?page=profile&a=1" method="post">
         	<tr><td rowspan="8" valign="top" style="background-image:url('img/Gast.gif'); background-size:120px 160px; width:120px; height:160px; background-repeat:no-repeat; background-position:center top;"><img src="img/border.png" height="160" /></td>
@@ -41,10 +47,10 @@ switch($_SESSION['et_lvl']) {
             <td><input name="firstname" value="<?= $_SESSION['et_firstname'];?>" class="login" /></td></tr>
             <tr><td>E-Mail: </td>
             <td><input name="email" value="<?= $_SESSION['et_email'];?>" class="login" /></td></tr>
-            <tr><td>Passwort: </td>
-            <td><input name="pass" value="" placeholder="Passwort" class="login" /></td></tr>
-            <tr><td>Wiederholen: </td>
-            <td><input name="pass2" value="" placehodler="Passwort" class="login" /></td></tr>
+            <tr><td>Passwort:* </td>
+            <td><input name="pass" type="password" value="" placeholder="Passwort" class="login" /></td></tr>
+            <tr><td>Wiederholen:* </td>
+            <td><input name="pass2" type="password" value="" placehodler="Passwort" class="login" /></td></tr>
             <tr><td colspan="2"><button type="submit">Speichern</button></td></tr>
             </form>
         </table>
